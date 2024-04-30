e1b0f4fe8f9e43e09847edd160dcc00bca941693
michael-shambel <Michaelshambe10@gmail.com>
2024-04-30 15:02:29 +0000

    task i dont

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -30,7 +30,7 @@ def get_place(place_id):
 
 @app_views.route('/api/v1/places/<place_id>', methods=['DELETE'])
 def delete_place(place_id):
-    place storage.get(Place, place_id)
+    place =  storage.get(Place, place_id)
     if place is None:
         return abort(404)
     place.delete()
