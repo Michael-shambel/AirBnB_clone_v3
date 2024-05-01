c540c106c3ed009429b245bcb14b2862fefca26f
michael-shambel <Michaelshambe10@gmail.com>
2024-05-01 14:24:21 +0000

    task 9

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -31,7 +31,8 @@ def delete_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    amenity.delete()
+    storage.delete(amenity)
+    storage.save()
     return jsonify({}), 200
 
 
