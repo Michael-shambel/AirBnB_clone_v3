88b5238e6d95f4005ec74778f8460a1b80f8c1c4
michael-shambel <Michaelshambe10@gmail.com>
2024-05-02 23:56:24 +0000

    task 1q

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 CORS(app, resources={r'api/v1/*': {'origins': '0.0.0.0'}})
 
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
