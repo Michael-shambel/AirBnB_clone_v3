42a135515d436b37eef78ff96708b8e82aa72e16
michael-shambel <Michaelshambe10@gmail.com>
2024-05-02 18:01:01 +0000

    task 2

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -93,9 +93,21 @@ class TestDBStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """Test method for to get data from db storage"""
-        pass
+        state = State(name="south")
+        state.save()
+        data_found = models.storage.get(State, state.id)
+        self.assertEqual(state, data_found)
+        for_none = models.storage.get(State, "none_id")
+        self.assertEqual(for_none, None)
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
         """Test method for count the instance"""
-        pass
+        state = State(name= "Ethiopia")
+        city = City(name= "Addis", state_id=new_state.id)
+        state.save()
+        city.save()
+        count_state = models.storage.count(State)
+        self.assertEqual(count_state, len(storage.all(State)))
+        count_all = storage.count()
+        self.assertEqual(count_all, len(mpdels.storage.all()))
