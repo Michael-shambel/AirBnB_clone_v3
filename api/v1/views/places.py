5daf753e302b1c39f7005568825339cd09ff8ff3
michael-shambel <Michaelshambe10@gmail.com>
2024-05-02 23:37:04 +0000

    task 8

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,8 +11,10 @@ from models.city import City
 from models.user import User
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'])
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
 def get_places_by_city(city_id):
+    """retrieves the list of all place"""
     city = storage.get(City, city_id)
     if city is None:
         return abort(404)
@@ -20,7 +22,8 @@ def get_places_by_city(city_id):
     return jsonify([place.to_dict() for place in places])
 
 
-@app_views.route('/places/<place_id>', methods=['GET'])
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_place(place_id):
     place = storage.get(Place, place_id)
     if place is None:
@@ -28,7 +31,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'])
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_place(place_id):
     place = storage.get(Place, place_id)
     if place is None:
@@ -38,8 +42,10 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'])
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
 def create_place(city_id):
+    """create a place object"""
     city = storage.get(City, city_id)
     if city is None:
         return abort(404)
@@ -60,12 +66,14 @@ def create_place(city_id):
     return jsonify(place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'])
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_place(place_id):
+    """update a place object"""
     place = storage.get(Place, place_id)
     if place is None:
         return abort(404)
-    if not request.json:
+    if not request.get_json():
         return abort(400, 'Not a JSON')
     data = request.get_json()
     for key, value in data.items():
